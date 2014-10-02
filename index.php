0c89194504cb28a504dec044c4eb0c4f0b4dd835
kobbybonsu <ohenekgyekye09@gmail.com>
2014-10-02 22:42:38 +0000

    initial css commit by Kwabena Ohene-bonsu
    css files and template

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>popSell-Out</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<table align="center">
+		<tr>
+			<td colspan="2" class="header">
+					<table align="center">
+						<tr>
+							<td>
+					 			<div align="left"><a href="index.php"><img src="popsell.png" alt="popsell-out" width="250" height="100">
+					 			</a></div>
+							</td>
+							<td width="867px"></td>
+						<td>
+							<!-- place AD button -->
+						<a href="new_sales.php" class="btn btn-primary">Post an AD!</a>
+						</td>
+					</tr>
+				</table>
+
+				</td>
+			</tr>
+      <!-- categories list-->
+			<tr>
+				<td>
+					<div id="divMenu">
+					    <ul>
+					        <li><a href="index.php">Home</a></li>
+					        <li><a href="#">Categories</a>
+					                <ul>
+					            <li><a href="#">Electronics</a></li>
+					            <li><a href="#">Food</a></li>
+					            <li><a href="#">Fashion</a></li>
+					            <li><a href="#">Services</a></li>
+					            <li><a href="#">Other</a></li>
+					        </ul></li>
+					        <li><a href="#">Campus Location</a>
+					                <ul>
+					            <li><a href="#">On-Campus Hostel</a></li>
+					            <li><a href="#">Colombiana</a></li>
+					            <li><a href="#">Dufie Platinum</a></li>
+					            <li><a href="#">Charlottes</a></li>
+					            <li><a href="#">Berekuso Township</a></li>
+					            <li><a href="#">Comet</a></li>
+					        </ul></li>
+					        <li><a href="#">Help & Support</a></li>
+					    </ul>
+					    </div>
+
+				</td>
+				<!-- <td width="0px"> </td> -->
+				<td>
+					<!--  search -->
+					<div align="right"><input class="textbox" type="text" placeholder="Search by product name or location... Eg. beads or Colombiana"></input>
+		  			<a class="btn btn-secondary"> Search </a></div>
+
+		  <!-- main content -->
+		<div align="middle" class="main">
+						<table>
+							<tr class="pagehead" align="right">
+								<td align="left"> ~All Products & Services~</td>
+								<td width="660"></td>
+							</tr>
+						</table>
+						<table width="90%" class="reportTable">
+							<tr class="header" >
+								<td width="330">Product Details</td>
+								<td width="30">Seller</td>
+								<td width="30">Contact</td>
+							</tr>
+
+						</table>
+
+				</div>
+			</td>
+		</tr>
+
+	</table>
+	
+		<!-- footer -->
+		<div class="footer">
+			popSell-Out Copyright 2014 | Powered by The A-Team.
+		</div>
+</body>
+</html>	
