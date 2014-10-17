84e4be167533505c934c86406ad141777e0b1049
esiyeenuwa <esi.yeenuwa@gmail.com>
2014-10-17 17:32:32 +0100

    Admin session creation

---
+++ Admin Session Creation/help.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Help & Support</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<table align="center">
+		<tr>
+			<td colspan="2" class="header">
+					<table align="center">
+						<tr>
+							<td>
+					 			<div align="left"><a href="index.php"><img src="popsell.png" alt="popsell-out" width="250" height="100">
+					 			</a></div>
+							</td>
+							<td width="867px"></td>
+						<td>
+
+							<!-- place AD button -->
+						<a href="new_sales.php" class="btn btn-primary">Post an AD!</a>
+						</td>
+					</tr>
+				</table>
+
+				</td>
+			</tr>
+      <!-- categories list-->
+      				<tr>
+      					<td>
+
+      				<!--  search -->
+					<table align="right">
+						<tr>
+							<td>
+					<form action="searchad.php" method="post">
+					<div align="right"><input class="textbox" type="text" name="search_text" placeholder="Search by product name or seller name">
+		  			<div class='btn btn-secondary'>
+		  				<input class='btn btn-secondary' type="submit" value="Search"></div>
+		  			</div>
+					</form>
+							</td>
+						</tr>
+					</table>
+						</td>
+					</tr>
+      				<tr>
+      					<table align="center">
+      						<tr>
+      							<td>
+					    <ul id="horiznav">
+					        <li><a href="index.php">Home</a></li>
+					        <li><a href="#">Categories</a>
+					                <ul>
+					            <li><a href="electronic.php">Electronics</a></li>
+					            <li><a href="food.php">Food</a></li>
+					            <li><a href="fashion.php">Fashion</a></li>
+					            <li><a href="services.php">Services</a></li>
+					            <li><a href="education.php">Education</a></li>
+					        </ul></li>
+					        <li><a href="#">Campus Location</a>
+					                <ul>
+					            <li><a href="oncampus.php">On-Campus Hostel</a></li>
+					            <li><a href="colombiana.php">Colombiana</a></li>
+					            <li><a href="dufie.php">Dufie Platinum</a></li>
+					            <li><a href="charlottes.php">Charlottes</a></li>
+					            <li><a href="masere.php">Masere</a></li>
+					            <li><a href="berekuso.php">Berekuso Town</a></li>
+					            <li><a href="comet.php">Comet</a></li>
+					            <li><a href="other.php">Other</a></li>
+					        </ul></li>
+					        <li><a href="help.php">Help & Support</a></li>
+					    </ul>
+					</td>
+						<td width="440px">
+						</td>
+				</tr>
+			</table>
+					
+				</tr>
+
+			<tr>
+				<td width="100px">
+
+				</td>
+				<td>
+
+		  <!-- main content -->
+		<div align="middle" class="main">
+						<table>
+							<tr class="pagehead" align="right">
+								<td align="left"> ~Help & Support~</td>
+								<td width="730"></td>
+							</tr>
+						</table>
+						<br>
+						<table width="90%" class="helptable">
+							<tr>
+								<td class="header">
+									All Comments, Questions and Feedback are Welcome
+								</td>
+							</tr>
+							<tr height="40">
+							</tr>
+							<tr>
+								<td align="center">
+									<iframe src="https://docs.google.com/forms/d/1Wdpkw0jqUVVn7pfEgqry9YRBQKSlUF3uE_A8j_o4U4A/viewform?embedded=true" width="760" height="880" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+								</td>
+							</tr>
+							<tr height="40">
+							</tr>
+							<tr>
+								<td>
+									Github Link: <b><i>https://github.com/ashesi-SE/A-team.git</i></b>
+								</td>
+							</tr>
+							<tr height="40">
+							</tr>
+							<tr>
+								<td align="center">
+									from the **A_Team**
+								</td>
+							</tr>
+						</table>
+						<br>
+						<br>
+
+				</div>
+			</td>
+		</tr>
+
+	</table>
+	
+		<!-- footer -->
+		<div class="footer">
+			popSell-Out Copyright 2014 | Powered by The A-Team.
+		</div>
+</body>
+</html>	
---
+++ Admin Session Creation/send_email.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<button>
+	<a href="mailto:someone@example.com?" target="_top">
+	Click to contact seller via Email</a>
+</button>
+
+</body>
+</html>
+
+
